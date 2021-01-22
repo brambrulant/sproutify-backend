91f198ec6b92d53bc342a75d6ded77a7699ba9f0
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-22 13:53:45 +0100

    added routes /tree for saving images of user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let request = require("request");
 let querystring = require("querystring");
+const user = require("./models/user");
 require("dotenv").config();
 
 let app = express();
@@ -39,10 +40,43 @@ app.get("/callback", function (req, res) {
     },
     json: true,
   };
-  request.post(authOptions, function (error, response, body) {
+  request.post(authOptions, async function (error, response, body) {
+    console.log(body);
+    // find or create the user
+
     var access_token = body.access_token;
-    res.redirect(frontEnd_uri + "?access_token=" + access_token);
+    // res.redirect(frontEnd_uri + "?access_token=" + access_token);
+    // add userID to redirect :
+    res.redirect(`${frontEnd_uri}?access_token=${access_token}`);
   });
+});
+
+app.post("/tree", function (req, res) {
+  console.log(req.body);
+
+  // {
+  //   userId: 123,
+  //   image: ' string',
+  //   name: string
+  // }
+});
+
+app.get("/:userId/tree", async function (req, res) {
+  // const trees = await return Tree.findAll({
+  //   where: {
+  //     userId: req.params.userId
+  //   }
+  // });
+  console.log(trees);
+});
+
+app.get("/:userId/tree/:treeId", function (req, res) {
+  // return Tree.find({
+  //   where : {
+  //     userId: req.params.userId,
+  //     id: req.params.treeId
+  //   }
+  // })
 });
 
 const port = process.env.PORT;
